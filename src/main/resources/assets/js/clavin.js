08d65a1c6ed55054b314a251e03041c9cb055d68
Jeremy Glesner <jeremy@bericotechnologies.com>
2017-02-08 09:58:35 -0500

    latest

--- src/main/resources/assets/js/clavin.js
+++ src/main/resources/assets/js/clavin.js
@@ -55,8 +55,88 @@ var Map = {
 	        zoomControlOptions: {
 	              position: google.maps.ControlPosition.RIGHT_TOP
 	        },
-	   		mapTypeId: google.maps.MapTypeId.ROADMAP
-	   	});
+	   		mapTypeId: google.maps.MapTypeId.ROADMAP,
+	   		styles: [
+	   	            {elementType: 'geometry', stylers: [{color: '#19233e'}]},
+	   	            {elementType: 'labels.text.stroke', stylers: [{color: '#19233e'}]},
+	   	            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+	   	            {
+	   	              featureType: 'administrative.locality',
+	   	              elementType: 'labels.text.fill',
+	   	              stylers: [{color: '#d59563'}]
+	   	            },
+	   	            {
+	   	              featureType: 'poi',
+	   	              elementType: 'labels.text.fill',
+	   	              stylers: [{color: '#d59563'}]
+	   	            },
+	   	            {
+	   	              featureType: 'poi.park',
+	   	              elementType: 'geometry',
+	   	              stylers: [{color: '#263c3f'}]
+	   	            },
+	   	            {
+	   	              featureType: 'poi.park',
+	   	              elementType: 'labels.text.fill',
+	   	              stylers: [{color: '#6b9a76'}]
+	   	            },
+	   	            {
+	   	              featureType: 'road',
+	   	              elementType: 'geometry',
+	   	              stylers: [{color: '#38414e'}]
+	   	            },
+	   	            {
+	   	              featureType: 'road',
+	   	              elementType: 'geometry.stroke',
+	   	              stylers: [{color: '#212a37'}]
+	   	            },
+	   	            {
+	   	              featureType: 'road',
+	   	              elementType: 'labels.text.fill',
+	   	              stylers: [{color: '#9ca5b3'}]
+	   	            },
+	   	            {
+	   	              featureType: 'road.highway',
+	   	              elementType: 'geometry',
+	   	              stylers: [{color: '#746855'}]
+	   	            },
+	   	            {
+	   	              featureType: 'road.highway',
+	   	              elementType: 'geometry.stroke',
+	   	              stylers: [{color: '#1f2835'}]
+	   	            },
+	   	            {
+	   	              featureType: 'road.highway',
+	   	              elementType: 'labels.text.fill',
+	   	              stylers: [{color: '#fcb040'}]  //  f3d19c
+	   	            },
+	   	            {
+	   	              featureType: 'transit',
+	   	              elementType: 'geometry',
+	   	              stylers: [{color: '#2f3948'}]
+	   	            },
+	   	            {
+	   	              featureType: 'transit.station',
+	   	              elementType: 'labels.text.fill',
+	   	              stylers: [{color: '#d59563'}]
+	   	            },
+	   	            {
+	   	              featureType: 'water',
+	   	              elementType: 'geometry',
+	   	              stylers: [{color: '#010623'}]
+	   	            },
+	   	            {
+	   	              featureType: 'water',
+	   	              elementType: 'labels.text.fill',
+	   	              stylers: [{color: '#515c6d'}]
+	   	            },
+	   	            {
+	   	              featureType: 'water',
+	   	              elementType: 'labels.text.stroke',
+	   	              stylers: [{color: '#17263c'}]
+	   	            }
+	   	          ]
+	   	});  //land 19233e, water 010623, footer.dark #222, sec-footer #1b1b1b,
 	   	
 	}	
 };
